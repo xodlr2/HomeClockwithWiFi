aae046c0d25df1e8d4437eed9f85e6992c643da8
unknown <xodlr2@gmail.com>
2021-01-22 17:56:53 +0900

    add font and parola library

--- HomeClockwithWiFi.ino
+++ HomeClockwithWiFi.ino
@@ -2,9 +2,22 @@
 #include <time.h>
 #include <Wire.h>
 #include <RTClib.h>
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+#include "NewFont.h"
+#include <Ticker.h>
 
-RTC_DS3231 rtc;
+/* prototype define */
+void loadRTC();
+void setRTC();
+int get_NTP();
+void printLocalTime();
+int connectingWiFi();
+void ledblink();
+void displayUpdate();
 
+/* Wifi define */
 const char* ssid = "genoray-3f-1";
 const char* password = "genoray12345";
 
@@ -15,6 +28,9 @@ int summerTime = 0;
 #define NUM_RETRY_WIFI 100
 #define NUM_RETRY_GET_NTP 100
 
+/* RTC define */
+RTC_DS3231 rtc;
+
 // char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 char daysOfTheWeek[7][12] = {"일", "월", "화", "수", "목", "금", "토"};
 // char hourAfterNoon[2][15] = {"Ante Meridiem", "Post Meridiem"};
@@ -24,7 +40,26 @@ int Year, Mon, Day;
 char* wDay;
 char* wAmPm;
 
-int loadRTC() {
+/* max7219 dot matrix define */
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+#define CS_PIN D8 // 15
+
+// Create a new instance of the MD_Parola class with hardware SPI connection:
+MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+
+// Setup for software SPI:
+// #define DATAPIN 2
+// #define CLK_PIN 4
+// MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+/* Ticker Timer define */
+Ticker timer1;
+Ticker timer2;
+Ticker timer3;
+//-----------------------------------------------------------------------------------------------
+
+void loadRTC() {
   DateTime now = rtc.now();
   Year = now.year();
   Mon = now.month();
@@ -62,7 +97,7 @@ void setRTC() {
   Min = ptm->tm_min;
   Sec = ptm->tm_sec;
   rtc.adjust(DateTime(Year, Mon, Day, Hour, Min, Sec));
-  Serial.printf("저장 시간: %d년 %d월 %d일(%s) ", Year+2000, Mon, Day, wDay);
+  Serial.printf("저장 시간: %d년 %d월 %d일(%s) ", Year + 2000, Mon, Day, wDay);
   Serial.printf("%d시 %d분 %d초\n", Hour, Min, Sec);
 }
 
@@ -114,32 +149,68 @@ int connectingWiFi() {
   return 1;
 }
 
+void ledblink() {
+  digitalWrite(LED_BUILTIN, !(digitalRead(LED_BUILTIN)));
+}
+
 void setup() {
 
-  //  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   Serial.begin(115200);
   Serial.setDebugOutput(true);
 
   rtc.begin();
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (connectingWiFi()) {
-    if (get_NTP()) {
-      printLocalTime();
-      setRTC();
+  /*
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (connectingWiFi()) {
+      if (get_NTP()) {
+        printLocalTime();
+        setRTC();
+      }
     }
-  }
-  else {
-    loadRTC();
-  }
-  WiFi.disconnect(true); //disconnect WiFi as it's no longer needed
-  WiFi.mode(WIFI_OFF);
+    WiFi.disconnect(true); //disconnect WiFi as it's no longer needed
+    WiFi.mode(WIFI_OFF);
+  */
+
+  // Intialize the object:
+  myDisplay.begin();
+  myDisplay.setFont(newFont);
+  // Set the intensity (brightness) of the display (0-15):
+  myDisplay.setIntensity(10);
+  // Clear the display:
+  // myDisplay.displayClear();
+
+  timer1.attach_ms(500, ledblink);
+  timer2.attach_ms(1000, loadRTC);
+  timer3.attach_ms(100, displayUpdate);
+}
+
+void displayUpdate() {
+  //  if (myDisplay.displayAnimate()) {
+  //    myDisplay.displayReset();
+  //  }
+  //  myDisplay.displayAnimate();
+
+  char buf[16];
+  sprintf(buf, "%d : %02d", Hour, Min);
+  //  myDisplay.displayText(buf, PA_CENTER, 0, 0, PA_PRINT, PA_NO_EFFECT);
+  //  myDisplay.displayAnimate();
+  //
+  //    if (myDisplay.displayAnimate()) {
+  ////        sprintf(buf, "%d:%02d %dC ", Hour, Min, Temp);
+  //  myDisplay.displayText("123:21 1234C", PA_LEFT, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  //    }
+
+  //  myDisplay.displayAnimate();
+
+  myDisplay.setTextAlignment(PA_CENTER);
+  myDisplay.printf("%s", buf);
+  //  Serial.printf("myDisplay %s \n", buf);
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  //  printLocalTime();
-  loadRTC();
-  delay(1000);
+
 }
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # HomeClockwithWiFi
 
-add RTClib library
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+보드 메니저 url
+https://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+#include <RTClib.h>
+add library for Real Time Clock Module
 https://github.com/adafruit/RTClib
 
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+add library for MAX7219 Matrix
+https://github.com/MajicDesigns/MD_Parola
+
+#include "NewFont.h"
+font editor
+https://pjrp.github.io/MDParolaFontEditor
